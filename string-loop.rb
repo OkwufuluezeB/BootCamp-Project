873ce1d08a3037a73273b31bdc27af1111253929
andela-gjames <george.james@andela.com>
2016-02-16 14:49:15 +0100

    create string loop with while

--- string-loop.rb
+++ string-loop.rb
@@ -4,6 +4,9 @@ my_string.upcase!
 
 my_list = my_string.split("")
 
-my_list.each do |item|
-	puts item
+counter = 0
+
+while counter < my_list.length do
+	puts "#{counter + 1}: #{my_list[counter]}"
+    counter += 1
 end
